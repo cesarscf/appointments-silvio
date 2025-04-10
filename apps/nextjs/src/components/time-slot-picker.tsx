7c1883926c1cfea8abe1cffd159c7f5b23e92f2a
cesar <cesarscfilho@gmail.com>
2025-04-10 17:14:12 -0300

    debug: test

--- apps/nextjs/src/components/time-slot-picker.tsx
+++ apps/nextjs/src/components/time-slot-picker.tsx
@@ -72,6 +72,7 @@ export function TimeSlotPicker({
             selected={date}
             onSelect={(newDate) => {
               if (newDate) {
+                console.log(date);
                 setDate(newDate);
               }
             }}
